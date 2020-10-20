ee4df70e6debc6bb58c5e63b08bb414ff96d9b25
vagrant <vagrant@homestead>
2020-10-20 07:26:21 +0000

    compatible with latest version

--- runlibrary.php
+++ runlibrary.php
@@ -2,5 +2,5 @@
 
 require_once 'vendor/autoload.php';
 
-$tarohida = new Tarohida\Sample\SampleClass();
-var_dump($tarohida->helloWorld());
+$tarohida = new Tarohida\Tarohida\Tarohida('I use tarohida/tarohida!');
+var_dump($tarohida->say());
